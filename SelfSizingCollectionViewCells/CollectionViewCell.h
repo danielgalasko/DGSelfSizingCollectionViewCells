ddca30f2cc25f7e385145c0285bc8109c034d7bc
Daniel Galasko <danielhgalasko@gmail.com>
2016-02-16 21:00:10 +0200

    added and commented out the implementation of layout attributes for testing

--- SelfSizingCollectionViewCells/CollectionViewCell.h
+++ SelfSizingCollectionViewCells/CollectionViewCell.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface CollectionViewCell : UICollectionViewCell
-@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (nonatomic, strong) NSString *text;
 @end
--- SelfSizingCollectionViewCells/CollectionViewCell.m
+++ SelfSizingCollectionViewCells/CollectionViewCell.m
@@ -8,6 +8,41 @@
 
 #import "CollectionViewCell.h"
 
+@interface CollectionViewCell()
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (nonatomic, assign) CGSize cachedSize;
+@end
+
 @implementation CollectionViewCell
 
+- (void)awakeFromNib {
+    self.cachedSize = CGSizeZero;
+}
+
+- (void)setText:(NSString *)text {
+    self.cachedSize = CGSizeZero;
+    self.textLabel.text = text;
+}
+
+- (NSString *)text {
+    return self.textLabel.text;
+}
+
+//- (UICollectionViewLayoutAttributes *)preferredLayoutAttributesFittingAttributes:(UICollectionViewLayoutAttributes *)layoutAttributes {
+//    
+//    UICollectionViewLayoutAttributes *attr = [layoutAttributes copy];
+//    if (CGSizeEqualToSize(CGSizeZero, self.cachedSize)) {
+//        self.frame = attr.frame;
+//        [self setNeedsLayout];
+//        [self layoutIfNeeded];
+//        CGFloat fontSize = [UIFontDescriptor preferredFontDescriptorWithTextStyle:UIFontTextStyleHeadline].pointSize * 1.4;
+//        self.textLabel.font = [UIFont fontWithName:[[UIFont fontNamesForFamilyName:self.textLabel.font.familyName] firstObject] size:fontSize];
+//        CGSize size = [self.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+//        self.cachedSize = size;
+//        CGRect newFrame = attr.frame;
+//        newFrame.size = size;
+//        attr.frame = newFrame;
+//    }
+//    return attr;
+//}
 @end
